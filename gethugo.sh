87dfa8e7aaa3a86ea4cb4e4bda85051df25564c2
mark-galich <mark@jinn.ee>
2020-10-08 19:06:39 +0300

    N 08 okt   2020 19:06:39 EEST

--- gethugo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /tmp
-wget https://github.com/gohugoio/hugo/releases/download/v0.76.2/hugo_0.76.2_Linux-64bit.tar.gz
-tar -zxvf hugo_0.76.2_Linux-64bit.tar.gz 
-sudo mv hugo /usr/local/bin/
-hugo version 
